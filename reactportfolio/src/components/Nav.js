c15c8fffb5c0d2c78e7b4dbb341fadef8edd1afc
Salman Ghouse <ghousemu@sheridancollege.ca>
2022-03-10 10:58:52 -0500

    finished nav component html

--- reactportfolio/src/components/Nav.js
+++ reactportfolio/src/components/Nav.js
@@ -1,1 +1,22 @@
+import React from "react";
 
+function Nav() {
+  return (
+    <header class="topnav">
+      <h1>Salman Ghouse</h1>
+      <nav>
+        <div id="myLinks">
+          <a href="#news">About Me</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">Portfolio</a>
+          <a href="#about">Resume</a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
