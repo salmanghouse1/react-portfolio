ee796e76e2cc8d021c4834dfe710a7e7c086c735
unknown <salmanghouse1@gmail.com>
2023-02-03 22:30:33 -0500

    added form

--- reactportfolio/src/components/pages/Contact.js
+++ reactportfolio/src/components/pages/Contact.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import styled from "styled-components";
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
-function Contact() {
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.SERVICE_ID,
+        process.env.TEMPLATE_ID,
+        form.current,
+        process.env.USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <section className="flex">
-      <h1 className="heading-block">Contact</h1>
-      <form>
-        <label>
-          username
-          <input type="text" />
-        </label>
-        <label>
-          email
-          <input type="email" />
-        </label>
-
-        <label>
-          Message:<input type="textarea"></input>
-        </label>
-        <button>Send</button>
+    <StyledContactForm>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
       </form>
-    </section>
+    </StyledContactForm>
   );
-}
+};
 
 export default Contact;
+
+
+
